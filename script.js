f2bf644f32f76040797125b060cccab06a302ee8
Om <ommukherjee28@gmail.com>
2021-11-16 00:15:01 +0530

    Added links

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const Question_json={
         "Question":"Which of the following sentences contain a word used in the above video but in a different meaning/context from the same?",
         "answer":"The biggest marathon is held through the Sahara Desert.",
         "options":{"a":"He was deserted by his father at the age of five.", "b":"The dessert is too sweet to be consumed daily.", "c":"The biggest marathon is held through the Sahara Desert.","d":"None of the above"},
-        "link":"https://firebasestorage.googleapis.com/v0/b/quast-17b61.appspot.com/o/Questions%2FQ1.mp4?alt=media&token=9e9dfbc2-78cc-4f19-a70b-f8ea6e112002"
+        "link":"https://firebasestorage.googleapis.com/v0/b/quaestio-17070.appspot.com/o/Questions%2Fq1.mp4?alt=media&token=2ff34399-763c-45b7-8099-1dcfb0a1d677"
       },
     },
     {
@@ -21,7 +21,7 @@ const Question_json={
       "Question":"Which of the following words is best suited to describe the situation in the video with context to the door with respect to the person?",
       "answer":"Wedged",
       "options":{"a":"Immobile", "b":"Lodged", "c":"Wedged","d":"Rooted"},
-      "link":"https://firebasestorage.googleapis.com/v0/b/quast-17b61.appspot.com/o/Questions%2FQ2.mp4?alt=media&token=e442f1af-614e-40f9-b714-57251af20600"
+      "link":"https://firebasestorage.googleapis.com/v0/b/quaestio-17070.appspot.com/o/Questions%2Fq2.mp4?alt=media&token=8e6fd0d8-0ec9-4304-97e1-9dd30f2c27c8"
       }
       
     },
@@ -45,7 +45,7 @@ const Question_json={
             "Question":"What is the closest synonym from among the following options to the word 'zing' as suggested by the use of it in the video?",
             "answer":"Avidity",
             "options":{"a":"Zest", "b":"Enthusiasm", "c":"Fervour","d":"Avidity"},
-            "link":"https://firebasestorage.googleapis.com/v0/b/quast-17b61.appspot.com/o/Questions%2FQ4.mp4?alt=media&token=69ecf0d8-c3be-4547-bee0-d434040f84d5"
+            "link":"https://firebasestorage.googleapis.com/v0/b/quaestio-17070.appspot.com/o/Questions%2Fq4.mp4?alt=media&token=08441496-d4fc-4690-87ee-5035f8dde162"
       }
 
     },
@@ -57,7 +57,7 @@ const Question_json={
             "Question":"Which of the following words best encapsulate the feeling of \"I don't like where this is going\" from the video?",
             "answer":"Foreboding",
             "options":{"a":"Foreboding", "b":"Omen", "c":"Auspice","d":"Presage"},
-            "link":"https://firebasestorage.googleapis.com/v0/b/quast-17b61.appspot.com/o/Questions%2FQ4.mp4?alt=media&token=69ecf0d8-c3be-4547-bee0-d434040f84d5"
+            "link":"https://firebasestorage.googleapis.com/v0/b/quaestio-17070.appspot.com/o/Questions%2Fq5.mp4?alt=media&token=0e53c9a0-e17a-4e25-ae6a-626b5bc23d29"
       }
 
     },
@@ -69,7 +69,7 @@ const Question_json={
             "Question":"The word 'Sutekh' from the video was coined to actually refer to what of the following?",
             "answer":"The Egyptian god of deserts, storm and foreigners",
             "options":{"a":"The Egyptian demon of disorders, floods and deaths", "b":"The Mesopotamian deity of Crops and Harvest", "c":"The Egyptian god of deserts, storm and foreigners","d":"None of the above"},
-            "link":"https://firebasestorage.googleapis.com/v0/b/quast-17b61.appspot.com/o/Questions%2FQ4.mp4?alt=media&token=69ecf0d8-c3be-4547-bee0-d434040f84d5"
+            "link":"https://firebasestorage.googleapis.com/v0/b/quaestio-17070.appspot.com/o/Questions%2Fq6.mp4?alt=media&token=458c4563-3fe4-4fd0-9052-f1ed45e5b59c"
       }
 
     },
@@ -81,7 +81,7 @@ const Question_json={
             "Question":"The man talking in the video can be said to be doing what from among the following verbs?",
             "answer":"Discombobulating",
             "options":{"a":"Poppycocking", "b":"Gobbledegooking", "c":"Flumoxing","d":"Discombobulating"},
-            "link":"https://firebasestorage.googleapis.com/v0/b/quast-17b61.appspot.com/o/Questions%2FQ4.mp4?alt=media&token=69ecf0d8-c3be-4547-bee0-d434040f84d5"
+            "link":"https://firebasestorage.googleapis.com/v0/b/quaestio-17070.appspot.com/o/Questions%2Fq7.mp4?alt=media&token=9dcac3fb-63b7-4f12-b20b-172f30e6b2ea"
       }
 
     },
@@ -93,7 +93,7 @@ const Question_json={
             "Question":"The word 'ghosting' as used in the video is a slang from the video gaming industry. What word from among the following is the closest to its actual meaning?",
             "answer":"Mooching",
             "options":{"a":"Mooching", "b":"Mousing", "c":"Sliding","d":"Skulking"},
-            "link":"https://firebasestorage.googleapis.com/v0/b/quast-17b61.appspot.com/o/Questions%2FQ4.mp4?alt=media&token=69ecf0d8-c3be-4547-bee0-d434040f84d5"
+            "link":"https://firebasestorage.googleapis.com/v0/b/quaestio-17070.appspot.com/o/Questions%2Fq8.mp4?alt=media&token=33596afc-8497-4a7d-b32e-ce250cc2bed9"
       }
 
     },
@@ -105,7 +105,7 @@ const Question_json={
             "Question":"The term 'Reich' is derived from the Germanic word which generally means _____________ in English but in German, it is typically used to designate a kingdom or an empire, especially the Roman Empire.Which word best fits the blank?",
             "answer":"Realm",
             "options":{"a":"Resistance", "b":"Revolution", "c":"Realm","d":"Insurgency"},
-            "link":"https://firebasestorage.googleapis.com/v0/b/quast-17b61.appspot.com/o/Questions%2FQ4.mp4?alt=media&token=69ecf0d8-c3be-4547-bee0-d434040f84d5"
+            "link":"https://firebasestorage.googleapis.com/v0/b/quaestio-17070.appspot.com/o/Questions%2Fq9.mp4?alt=media&token=7829712b-fc7f-40ff-a743-14ae8175c59e"
       }
 
     },
@@ -117,7 +117,7 @@ const Question_json={
             "Question":"Isaac Asimov, a highly respected and celebrated English writer wrote which of the following famous literary works in the year 1950?",
             "answer":"I, Robot",
             "options":{"a":"I, Robot", "b":"Dandelion wine", "c":"Stranger in a strange land","d":"Starship troopers"},
-            "link":"https://firebasestorage.googleapis.com/v0/b/quast-17b61.appspot.com/o/Questions%2FQ4.mp4?alt=media&token=69ecf0d8-c3be-4547-bee0-d434040f84d5"
+            "link":"https://firebasestorage.googleapis.com/v0/b/quaestio-17070.appspot.com/o/Questions%2Fq10.mp4?alt=media&token=10866eb9-c683-43d6-9702-4cbf5c83b535"
       }
 
     },
@@ -129,7 +129,7 @@ const Question_json={
             "Question":"What does the word 'transience' as used here mean?",
             "answer":"the state or fact of lasting over a long period of time",
             "options":{"a":"the state or fact of lasting only for a short time", "b":"eternity", "c":"the state or fact of lasting over a long period of time","d":"mortality"},
-            "link":"https://firebasestorage.googleapis.com/v0/b/quast-17b61.appspot.com/o/Questions%2FQ4.mp4?alt=media&token=69ecf0d8-c3be-4547-bee0-d434040f84d5"
+            "link":"https://firebasestorage.googleapis.com/v0/b/quaestio-17070.appspot.com/o/Questions%2Fq11.mp4?alt=media&token=56cf5f55-854a-4bb3-9fe6-ca0744d70b9e"
       }
 
     },
@@ -141,7 +141,7 @@ const Question_json={
             "Question":"Judging from the theme portrayed and the dialogues used in the above video, what does the word 'euthymia' mean from the phrase used in the video 'plane of euthymia'",
             "answer":"the state of living without mood disturbances",
             "options":{"a":"the physical manifestation of lightning", "b":"the state of living without any changes around you", "c":"the expression of  power in a subtle way","d":"the state of living without mood disturbances"},
-            "link":"https://firebasestorage.googleapis.com/v0/b/quast-17b61.appspot.com/o/Questions%2FQ4.mp4?alt=media&token=69ecf0d8-c3be-4547-bee0-d434040f84d5"
+            "link":"https://firebasestorage.googleapis.com/v0/b/quaestio-17070.appspot.com/o/Questions%2Fq12.mp4?alt=media&token=c3ecf1a8-df6c-417e-ac13-2d361b3fbed7"
       }
 
     },
@@ -153,7 +153,7 @@ const Question_json={
             "Question":"Taking an example from the shown video, what does the term 'to eulogize' means?",
             "answer":"To summarize in the form of literature or text in a praiseworthy manner",
             "options":{"a":"To summarize in the form of literature or text with no biases or prejudices", "b":"To summarize in the form of literature or text in a praiseworthy manner", "c":"To summarize in the form of literature or text with negative biases or prejudices","d":"To summarize in the form of literature or text in a derogatory manner"},
-            "link":"https://firebasestorage.googleapis.com/v0/b/quast-17b61.appspot.com/o/Questions%2FQ4.mp4?alt=media&token=69ecf0d8-c3be-4547-bee0-d434040f84d5"
+            "link":"https://firebasestorage.googleapis.com/v0/b/quaestio-17070.appspot.com/o/Questions%2Fq13.mp4?alt=media&token=5fadb3ad-68d3-4ca5-947c-a2482ff55f91"
       }
 
     },
@@ -165,7 +165,7 @@ const Question_json={
             "Question":"What does the word used in the video 'perturbation' mean?",
             "answer":"Uneasiness",
             "options":{"a":"Judgement", "b":"Subjects", "c":"Uneasiness","d":"Execution"},
-            "link":"https://firebasestorage.googleapis.com/v0/b/quast-17b61.appspot.com/o/Questions%2FQ4.mp4?alt=media&token=69ecf0d8-c3be-4547-bee0-d434040f84d5"
+            "link":"https://firebasestorage.googleapis.com/v0/b/quaestio-17070.appspot.com/o/Questions%2Fq14.mp4?alt=media&token=2ed3ab2b-4ed2-49b6-9529-5fa82d65c0da"
       }
 
     },
@@ -177,7 +177,7 @@ const Question_json={
             "Question":"Derived from the Old French word 'folie', what does the words 'folly' mean in English in context of its use in the video?",
             "answer":"Foolishness",
             "options":{"a":"Foolishness", "b":"Madness", "c":"Uprising","d":"Revolution"},
-            "link":"https://firebasestorage.googleapis.com/v0/b/quast-17b61.appspot.com/o/Questions%2FQ4.mp4?alt=media&token=69ecf0d8-c3be-4547-bee0-d434040f84d5"
+            "link":"https://firebasestorage.googleapis.com/v0/b/quaestio-17070.appspot.com/o/Questions%2Fq15.mp4?alt=media&token=b6eb0cb3-5b59-4734-95a3-5fafec6d9c20"
       }
 
     },
@@ -189,7 +189,7 @@ const Question_json={
             "Question":"What does the term 'dregs' mean with context to its use in the video?",
             "answer":"the most worthless parts of something",
             "options":{"a":"outcasts rejected by the society", "b":"the most worthless parts of something", "c":"sediment contained in a liquid","d":"people looked down upon by others"},
-            "link":"https://firebasestorage.googleapis.com/v0/b/quast-17b61.appspot.com/o/Questions%2FQ4.mp4?alt=media&token=69ecf0d8-c3be-4547-bee0-d434040f84d5"
+            "link":"https://firebasestorage.googleapis.com/v0/b/quaestio-17070.appspot.com/o/Questions%2Fq16.mp4?alt=media&token=f18a4e89-bc08-4c54-9404-c0b6eb4848f2"
       }
 
     }
