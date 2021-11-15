1fabde43bc9c53c0175f1ae77464f486ab33aa9d
Om <ommukherjee28@gmail.com>
2021-11-15 20:50:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,334 @@
+const Question_json={
+  "questions":
+  [
+    {  
+      
+                  // QUESTION 1
+
+      "Question_obj":{
+          "id":0,
+        "Question":"Which of the following sentences contain a word used in the above video but in a different meaning/context from the same?",
+        "answer":"The biggest marathon is held through the Sahara Desert.",
+        "options":{"a":"He was deserted by his father at the age of five.", "b":"The dessert is too sweet to be consumed daily.", "c":"The biggest marathon is held through the Sahara Desert.","d":"None of the above"},
+        "link":"https://firebasestorage.googleapis.com/v0/b/quast-17b61.appspot.com/o/Questions%2FQ1.mp4?alt=media&token=9e9dfbc2-78cc-4f19-a70b-f8ea6e112002"
+      },
+    },
+    {
+                  // QUESTION 2
+
+      "Question_obj":{
+        "id":1,
+      "Question":"Which of the following words is best suited to describe the situation in the video with context to the door with respect to the person?",
+      "answer":"Wedged",
+      "options":{"a":"Immobile", "b":"Lodged", "c":"Wedged","d":"Rooted"},
+      "link":"https://firebasestorage.googleapis.com/v0/b/quast-17b61.appspot.com/o/Questions%2FQ2.mp4?alt=media&token=e442f1af-614e-40f9-b714-57251af20600"
+      }
+      
+    },
+    {
+      // QUESTION 3
+
+      "Question_obj":{
+            "id":2,
+            "Question":"What is the act of being thrown out of a window as such called?",
+            "answer":"Defenestration",
+            "options":{"a":"Defenestration", "b":"Bashment", "c":"Catoptromancy","d":"Discombobulate"},
+            "link":"https://firebasestorage.googleapis.com/v0/b/quast-17b61.appspot.com/o/Questions%2FQ3.mov?alt=media&token=c78c18e0-bff1-47c9-ac0b-f6b42906008f"
+      }
+
+    },
+    {
+      // QUESTION 4
+
+      "Question_obj":{
+            "id":3,
+            "Question":"What is the closest synonym from among the following options to the word 'zing' as suggested by the use of it in the video?",
+            "answer":"Avidity",
+            "options":{"a":"Zest", "b":"Enthusiasm", "c":"Fervour","d":"Avidity"},
+            "link":"https://firebasestorage.googleapis.com/v0/b/quast-17b61.appspot.com/o/Questions%2FQ4.mp4?alt=media&token=69ecf0d8-c3be-4547-bee0-d434040f84d5"
+      }
+
+    },
+    {
+      // QUESTION 5 - Link Updation Left
+
+      "Question_obj":{
+            "id":4,
+            "Question":"Which of the following words best encapsulate the feeling of \"I don't like where this is going\" from the video?",
+            "answer":"Foreboding",
+            "options":{"a":"Foreboding", "b":"Omen", "c":"Auspice","d":"Presage"},
+            "link":"https://firebasestorage.googleapis.com/v0/b/quast-17b61.appspot.com/o/Questions%2FQ4.mp4?alt=media&token=69ecf0d8-c3be-4547-bee0-d434040f84d5"
+      }
+
+    },
+    {
+      // QUESTION 6 - Link Updation Left
+
+      "Question_obj":{
+            "id":5,
+            "Question":"The word 'Sutekh' from the video was coined to actually refer to what of the following?",
+            "answer":"The Egyptian god of deserts, storm and foreigners",
+            "options":{"a":"The Egyptian demon of disorders, floods and deaths", "b":"The Mesopotamian deity of Crops and Harvest", "c":"The Egyptian god of deserts, storm and foreigners","d":"None of the above"},
+            "link":"https://firebasestorage.googleapis.com/v0/b/quast-17b61.appspot.com/o/Questions%2FQ4.mp4?alt=media&token=69ecf0d8-c3be-4547-bee0-d434040f84d5"
+      }
+
+    },
+    {
+      // QUESTION 7 - Link Updation Left
+
+      "Question_obj":{
+            "id":6,
+            "Question":"The man talking in the video can be said to be doing what from among the following verbs?",
+            "answer":"Discombobulating",
+            "options":{"a":"Poppycocking", "b":"Gobbledegooking", "c":"Flumoxing","d":"Discombobulating"},
+            "link":"https://firebasestorage.googleapis.com/v0/b/quast-17b61.appspot.com/o/Questions%2FQ4.mp4?alt=media&token=69ecf0d8-c3be-4547-bee0-d434040f84d5"
+      }
+
+    },
+    {
+      // QUESTION 8 - Link Updation Left
+
+      "Question_obj":{
+            "id":7,
+            "Question":"The word 'ghosting' as used in the video is a slang from the video gaming industry. What word from among the following is the closest to its actual meaning?",
+            "answer":"Mooching",
+            "options":{"a":"Mooching", "b":"Mousing", "c":"Sliding","d":"Skulking"},
+            "link":"https://firebasestorage.googleapis.com/v0/b/quast-17b61.appspot.com/o/Questions%2FQ4.mp4?alt=media&token=69ecf0d8-c3be-4547-bee0-d434040f84d5"
+      }
+
+    },
+    {
+      // QUESTION 9 - Link Updation Left
+
+      "Question_obj":{
+            "id":8,
+            "Question":"The term 'Reich' is derived from the Germanic word which generally means _____________ in English but in German, it is typically used to designate a kingdom or an empire, especially the Roman Empire.Which word best fits the blank?",
+            "answer":"Realm",
+            "options":{"a":"Resistance", "b":"Revolution", "c":"Realm","d":"Insurgency"},
+            "link":"https://firebasestorage.googleapis.com/v0/b/quast-17b61.appspot.com/o/Questions%2FQ4.mp4?alt=media&token=69ecf0d8-c3be-4547-bee0-d434040f84d5"
+      }
+
+    },
+    {
+      // QUESTION 10 - Link Updation Left
+
+      "Question_obj":{
+            "id":9,
+            "Question":"Isaac Asimov, a highly respected and celebrated English writer wrote which of the following famous literary works in the year 1950?",
+            "answer":"I, Robot",
+            "options":{"a":"I, Robot", "b":"Dandelion wine", "c":"Stranger in a strange land","d":"Starship troopers"},
+            "link":"https://firebasestorage.googleapis.com/v0/b/quast-17b61.appspot.com/o/Questions%2FQ4.mp4?alt=media&token=69ecf0d8-c3be-4547-bee0-d434040f84d5"
+      }
+
+    },
+    {
+      // QUESTION 11 - Link Updation Left
+
+      "Question_obj":{
+            "id":10,
+            "Question":"What does the word 'transience' as used here mean?",
+            "answer":"the state or fact of lasting over a long period of time",
+            "options":{"a":"the state or fact of lasting only for a short time", "b":"eternity", "c":"the state or fact of lasting over a long period of time","d":"mortality"},
+            "link":"https://firebasestorage.googleapis.com/v0/b/quast-17b61.appspot.com/o/Questions%2FQ4.mp4?alt=media&token=69ecf0d8-c3be-4547-bee0-d434040f84d5"
+      }
+
+    },
+    {
+      // QUESTION 12 - Link Updation Left 
+
+      "Question_obj":{
+            "id":11,
+            "Question":"Judging from the theme portrayed and the dialogues used in the above video, what does the word 'euthymia' mean from the phrase used in the video 'plane of euthymia'",
+            "answer":"the state of living without mood disturbances",
+            "options":{"a":"the physical manifestation of lightning", "b":"the state of living without any changes around you", "c":"the expression of  power in a subtle way","d":"the state of living without mood disturbances"},
+            "link":"https://firebasestorage.googleapis.com/v0/b/quast-17b61.appspot.com/o/Questions%2FQ4.mp4?alt=media&token=69ecf0d8-c3be-4547-bee0-d434040f84d5"
+      }
+
+    },
+    {
+      // QUESTION 13 - Link Updation Left
+
+      "Question_obj":{
+            "id":12,
+            "Question":"Taking an example from the shown video, what does the term 'to eulogize' means?",
+            "answer":"To summarize in the form of literature or text in a praiseworthy manner",
+            "options":{"a":"To summarize in the form of literature or text with no biases or prejudices", "b":"To summarize in the form of literature or text in a praiseworthy manner", "c":"To summarize in the form of literature or text with negative biases or prejudices","d":"To summarize in the form of literature or text in a derogatory manner"},
+            "link":"https://firebasestorage.googleapis.com/v0/b/quast-17b61.appspot.com/o/Questions%2FQ4.mp4?alt=media&token=69ecf0d8-c3be-4547-bee0-d434040f84d5"
+      }
+
+    },
+    {
+      // QUESTION 14 - Link Updation Left
+
+      "Question_obj":{
+            "id":13,
+            "Question":"What does the word used in the video 'perturbation' mean?",
+            "answer":"Uneasiness",
+            "options":{"a":"Judgement", "b":"Subjects", "c":"Uneasiness","d":"Execution"},
+            "link":"https://firebasestorage.googleapis.com/v0/b/quast-17b61.appspot.com/o/Questions%2FQ4.mp4?alt=media&token=69ecf0d8-c3be-4547-bee0-d434040f84d5"
+      }
+
+    },
+    {
+      // QUESTION 15 - Link Updation Left
+
+      "Question_obj":{
+            "id":14,
+            "Question":"Derived from the Old French word 'folie', what does the words 'folly' mean in English in context of its use in the video?",
+            "answer":"Foolishness",
+            "options":{"a":"Foolishness", "b":"Madness", "c":"Uprising","d":"Revolution"},
+            "link":"https://firebasestorage.googleapis.com/v0/b/quast-17b61.appspot.com/o/Questions%2FQ4.mp4?alt=media&token=69ecf0d8-c3be-4547-bee0-d434040f84d5"
+      }
+
+    },
+    {
+      // QUESTION 16 - Link Updation Left
+
+      "Question_obj":{
+            "id":15,
+            "Question":"What does the term 'dregs' mean with context to its use in the video?",
+            "answer":"the most worthless parts of something",
+            "options":{"a":"outcasts rejected by the society", "b":"the most worthless parts of something", "c":"sediment contained in a liquid","d":"people looked down upon by others"},
+            "link":"https://firebasestorage.googleapis.com/v0/b/quast-17b61.appspot.com/o/Questions%2FQ4.mp4?alt=media&token=69ecf0d8-c3be-4547-bee0-d434040f84d5"
+      }
+
+    }
+  ]
+    
+}
+
+var JsonQ=[];
+JsonQ.push.apply(JsonQ, Question_json.questions);
+var score=0;
+var l=Question_json.questions.length;
+var answered=false;
+var rand_num;
+
+//Function gets called in the beginning 
+window.onload = function() {
+  document.getElementById('score').innerHTML =score;
+  console.log('Total Length of the json file '+l);
+  new_question();
+
+}
+function arrayRemove(arr, value) { 
+    
+  return arr.filter(function(ele){ 
+      return ele != value; 
+  });
+}
+
+function new_question() {
+
+    rand_num=Math.floor(Math.random() * l);
+    console.log(JsonQ.length);
+    console.log(Question_json.questions.length);
+    //DEBUGGING
+    console.log('Random Number :'+rand_num);
+    console.log('Answer :'+JsonQ[rand_num].Question_obj.answer);
+
+    document.querySelector('.question').innerHTML=JsonQ[rand_num].Question_obj.Question;
+
+    document.querySelector('.video_screen').src=JsonQ[rand_num].Question_obj.link;
+
+    optionA=document.querySelector('#opt_a');
+    optionA.innerHTML=JsonQ[rand_num].Question_obj.options.a;
+
+    optionB=document.querySelector('#opt_b');
+    optionB.innerHTML=JsonQ[rand_num].Question_obj.options.b;
+
+    optionC=document.querySelector('#opt_c');
+    optionC.innerHTML=JsonQ[rand_num].Question_obj.options.c;
+
+    optionD=document.querySelector('#opt_d');
+    optionD.innerHTML=JsonQ[rand_num].Question_obj.options.d;
+    checkAnswer();
+
+}
+
+function checkAnswer(){
+
+  var opts = document.querySelector('.options');
+  opts.addEventListener('click', function(ev) {
+    if(answered==false)
+    {
+      
+      if (ev.target.innerHTML === JsonQ[rand_num].Question_obj.answer) {
+        answered = true;
+        console.log('Correct');
+        score++;
+        document.getElementById('score').innerHTML =score;
+        ev.target.style.backgroundColor='green';
+        
+        message=document.getElementById('message')
+        message.innerHTML = 'Correct!';
+        message.style.color='green';
+
+        if(JsonQ.length==1)
+        {
+          document.getElementById('finished').style.display='block';
+          JsonQ=[];
+          JsonQ.push.apply(JsonQ, Question_json.questions);
+          l=JsonQ.length;
+        }
+        else{
+          JsonQ.splice(rand_num,1);
+          l--;
+        }
+        
+      }
+      else if (!(ev.target.className==='options'))
+      {
+        answered = true;
+        console.log('Wrong Answer');
+        score=0;
+        document.getElementById('score').innerHTML =score;
+        ev.target.style.backgroundColor='red';
+
+        message=document.getElementById('message')
+        message.innerHTML = 'Wrong!';
+        message.style.color='red';
+        JsonQ=Question_json.questions;
+      }
+      else {
+        console.log('something else');
+      }
+    }
+    
+  },false);
+}
+function restart(){
+  score=0;
+  document.getElementById('finished').style.display='hidden';
+  document.getElementById('score').innerHTML =score;
+  answered=false;
+  document.getElementById('message').innerHTML="";
+  JsonQ=[];
+  JsonQ.push.apply(JsonQ, Question_json.questions);
+  l=JsonQ.length;
+
+  var options = document.getElementsByClassName("grid-item");
+  var i;
+  for (i = 0; i < options.length; i++) {
+      options[i].style.backgroundColor="rgb(255, 204, 0)";
+  }
+
+  new_question();
+}
+function next_question(){
+  if (answered==true){
+    
+    document.getElementById('finished').style.display='hidden';
+    answered=false;
+    document.getElementById('message').innerHTML="";
+    var options = document.getElementsByClassName("grid-item");
+    var i;
+    for (i = 0; i < options.length; i++) {
+        options[i].style.backgroundColor="rgb(255, 204, 0)";
+    }
+    document.getElementById('score').innerHTML =score;
+    new_question();
+  }
+
+  
+}
